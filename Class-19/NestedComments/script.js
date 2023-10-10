4ea79cc93cb7f4a28068debb2bb763bb6990083d
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2023-10-10 23:10:33 +0530

    event delegation

--- Class-19/NestedComments/script.js
+++ Class-19/NestedComments/script.js
@@ -10,21 +10,42 @@ const createInputBox = () => {
     return div
 }
 
+const createReply = (text) => {
+    let div = document.createElement('div')
+    div.setAttribute('class', 'all-comments')
+
+    div.innerHTML = `<div class="card">
+        <span class="text">${text}</span>
+        <span id="reply" class="reply">Add reply</span>
+    </div>`
+
+    return div
+}
+
 commentContainer.addEventListener('click', event => {
     let replyBtnClicked = event.target.classList.contains('reply')
     let submitBtnClicked = event.target.classList.contains('submit')
 
-
+    // 1. Find the closest comment
+    let closestCard = event.target.closest('.all-comments')
 
     if (replyBtnClicked) {
         // add the reply section
-        // 1. Find the closest comment
-        let closestCard = event.target.closest('.all-comments')
+
         // 2. Add reply section to closestCard
         let inputBox = createInputBox()
         closestCard.appendChild(inputBox)
     } 
     if (submitBtnClicked) {
         // append the reply to the comment
+        let commentClosest = event.target.closest('.comment-reply-section')
+        let inputSection = commentClosest.children[0]
+        let inputText = inputSection.value
+
+        if (inputText) {
+            let replyComment = createReply(inputText)
+            closestCard.appendChild(replyComment)
+            commentClosest.remove()
+        }
     }
 })
