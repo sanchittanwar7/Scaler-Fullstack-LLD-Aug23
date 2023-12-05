9e16238935037e72971a73993b48b01faf56cc3c
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2023-12-05 22:17:46 +0530

    adds reducers

---
+++ adv_hooks/src/components/reducer.js
@@ -0,0 +1,38 @@
+import uuid from 'react-uuid';
+
+const taskReducer = (state, action) => {
+    switch(action.type) {
+        case "ADD_TASK" : {
+            const newTask = {...action.payload, "id": uuid(), "isDone": false}
+            return [...state, newTask];
+        }
+
+        case "REMOVE_TASK" : {
+            const filteredTask = state.filter((task) => task.id !== action.payload);
+            return [...filteredTask];
+        }
+
+        case "DONE_TASK" : {
+            const index = state.findIndex((task) => task.id === action.payload);
+            const doneTask = [...state];
+            doneTask[index].isDone = true;
+            return doneTask;
+        }
+
+        default : return state
+    }
+}
+
+const formReducer = (state, action) => {
+    switch(action.type) {
+        case "HANDLE_TASK": {
+            const key = action.field;
+            const value = action.payload;
+            return {...state, [key]: value};
+        }
+
+        default : return state
+    }
+}
+
+export {taskReducer, formReducer}
