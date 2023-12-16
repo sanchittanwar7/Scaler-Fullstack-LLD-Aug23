962e82b95995392ab995e0f73c4e74d223d0854a
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2023-12-16 23:18:51 +0530

    middlewares

--- NodeJS-2-express/index.js
+++ NodeJS-2-express/index.js
@@ -4,6 +4,35 @@ const app = express()
 
 const PORT = 8080
 
+const loggingMiddleware = (req, res, next) => {
+    console.log("This is logging middleware")
+
+    next()
+}
+
+const authMiddleware = (req, res, next) => {
+    const { password } = req.query
+
+    if (password == 123) {
+        next()
+    } else {
+        res.sendStatus(401)
+    }
+}
+
+app.use(loggingMiddleware)
+// app.use(authMiddleware)
+
+const staticMiddleware = express.static('public')
+app.use(staticMiddleware)
+
+app.get('/users/:userId', authMiddleware, (req, res) => {
+    const param = req.params
+    const query = req.query
+    console.log(query)
+    res.send(`Response for ${param.userId} and ${query.id}`)
+})
+
 app.get('/', (req, res) => {
     console.log('On route /')
     res.send("Response from get")
@@ -16,6 +45,8 @@ app.post('/', (req, res) => {
 app.delete('/', (req, res) => {
     res.send("Response from delete")
 })
+
+
 
 
 app.listen(PORT, () => {
