05050d48d76ec86d8d29121aba56bbfc52043ad3
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2023-12-19 22:55:57 +0530

    patch put delete endpoint

--- NodeJS3-REST/index.js
+++ NodeJS3-REST/index.js
@@ -26,6 +26,45 @@ app.post('/products', (req, res) => {
     res.status(200).json(product)
 })
 
+// DELETE endpoint
+app.delete('/products/:pid', (req, res) => {
+    const { pid } = req.params
+    const productIndex = data.products.findIndex(p => p.id == pid)
+    const product = data.products[productIndex]
+    data.products.splice(productIndex, 1)
+    fs.writeFileSync('data.json', JSON.stringify(data), 'utf-8')
+    res.status(200).json(product)
+})
+
+// PUT endpoint
+app.put('/products', (req, res) => {
+    const product = req.body
+    const productIndex = data.products.findIndex(p => p.id == product.id)
+    console.log(productIndex)
+    if (productIndex === undefined) {
+        // product is not there in database
+        // insert the product
+        data.products.push(product)
+    } else {
+        // data.products.splice(productIndex, 1, {...product})
+        data.products[productIndex] = product
+    }
+    fs.writeFileSync('data.json', JSON.stringify(data), 'utf-8')
+    res.status(200).json(product)
+})
+
+// PATCH endpoint
+app.patch('/products', (req, res) => {
+    const product = req.body
+    const productIndex = data.products.findIndex(p => p.id == product.id)
+    const originalProduct = data.products[productIndex]
+    // picking properties from existing product, and overriding it with new ones from req body
+    const updatedProduct = {...originalProduct, ...product}
+    data.products[productIndex] = updatedProduct
+    fs.writeFileSync('data.json', JSON.stringify(data), 'utf-8')
+    res.status(200).json(updatedProduct)
+})
+
 app.listen(8080, () => {
     console.log("Server has started")
 })
