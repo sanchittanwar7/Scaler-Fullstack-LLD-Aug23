144dd7cf62c7c0ab47fe2a8463e5dcb357eeda4c
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2023-12-19 22:26:42 +0530

    get post endpoints

--- NodeJS3-REST/index.js
+++ NodeJS3-REST/index.js
@@ -1,3 +1,32 @@
 const express = require('express')
+const fs = require("fs")
+
+const data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'))
 
 const app = express()
+
+app.use(express.json())
+
+// GET endpoint
+app.get('/products', (req, res) => {
+    res.send(data)
+})
+
+app.get('/products/:pid', (req, res) => {
+    const { pid } = req.params
+    const product = data.products.filter(p => pid == p.id)
+    res.send(product[0])
+})
+
+// POST endpoint
+app.post('/products', (req, res) => {
+    const product = req.body
+    data.products.push(product)
+    fs.writeFileSync('data.json', JSON.stringify(data), 'utf-8')
+    res.status(200).json(product)
+})
+
+app.listen(8080, () => {
+    console.log("Server has started")
+})
+
