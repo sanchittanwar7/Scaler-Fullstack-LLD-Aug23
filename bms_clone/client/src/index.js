56e6d9a838e979dcf8cb549b1b390fc08d1715d6
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2024-01-06 21:35:08 +0530

    redux boilerplate

--- bms_clone/client/src/index.js
+++ bms_clone/client/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
